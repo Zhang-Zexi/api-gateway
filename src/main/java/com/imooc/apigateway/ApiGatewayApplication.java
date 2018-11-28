4a725dce1a232943772de0db88b06d8b86f631af
zhangzx <zhangzx@allinfinance.com>
2018-11-28 14:22:17 +0800

    Zuul路由和Cookie的使用

--- src/main/java/com/imooc/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/imooc/apigateway/ApiGatewayApplication.java
@@ -2,7 +2,10 @@ package com.imooc.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,4 +14,11 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+
 }
